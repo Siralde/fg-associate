397746012638fd36855c3a7a9f7959544ed740d8
Aldemaro <aj.gonzalez@soprasteria.com>
2019-07-19 14:50:57 +0200

    Nueva actualizacion

--- src/app/pages/contact/contact.page.ts
+++ src/app/pages/contact/contact.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import info from '../../../assets/i18n/es.json';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.page.scss'],
 })
 export class ContactPage implements OnInit {
+  public facebook: string;
+  public twitter: string;
+  public instagram: string;
+  public dir1: string;
+  public dir2: string;
+  public tel1: string;
+  public tel2: string;
+  public tel3: string;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.facebook = info.CONTENTS_MENU.CONTACTS.FACEBOOK;
+    this.twitter = info.CONTENTS_MENU.CONTACTS.TWITTER;
+    this.instagram = info.CONTENTS_MENU.CONTACTS.INSTAGRAM;
+    this.dir1 = info.CONTENTS_MENU.CONTACTS.DIR_1;
+    this.dir2 = info.CONTENTS_MENU.CONTACTS.DIR_2;
+    this.tel1 = info.CONTENTS_MENU.CONTACTS.TELEF_1;
+    this.tel2 = info.CONTENTS_MENU.CONTACTS.TELEF_2;
+    this.tel3 = info.CONTENTS_MENU.CONTACTS.TELEF_3;
   }
-
 }
